8d2ddb617077b12ac62a087b13bc04bbe27eb996
wanumair <wanumair1209@gmail.com>
2024-07-09 22:40:55 +0800

    get content using request

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import streamlit as st
+from download_image import download_image
+api_key = "Brc7oHWxGqGj6uMMHeOyACne8HtH8HQC6f3UPh7h"
+
+url = ("https://api.nasa.gov/planetary/apod?"
+       f"api_key={api_key}")
+
+request = requests.get(url)
+
+content = request.json()
+print(content)
+
+# image_url = content['url']
+
+#streamlit display content
+# st.title(content['title'])
+#
+# image = download_image(image_url)
+# st.image(image)
+#
+# st.write(content["explanation"])
+
